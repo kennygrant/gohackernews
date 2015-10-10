6dc5560ca7cd13b2f6e53db1bb2251746172c68f
Kenny Grant <kennygrant@gmail.com>
2015-10-11 00:42:59 +0100

    added story list to users pages

--- src/users/actions/show.go
+++ src/users/actions/show.go
@@ -5,6 +5,7 @@ import (
 	"github.com/fragmenta/view"
 
 	"github.com/kennygrant/gohackernews/src/comments"
+	"github.com/kennygrant/gohackernews/src/stories"
 	"github.com/kennygrant/gohackernews/src/users"
 )
 
@@ -21,8 +22,15 @@ func HandleShow(context router.Context) error {
 	}
 
 	// Get the user comments
-	q := comments.Where("user_id=?", user.Id).Limit(100).Order("created_at desc")
+	q := comments.Where("user_id=?", user.Id).Limit(10).Order("created_at desc")
 	userComments, err := comments.FindAll(q)
+	if err != nil {
+		return router.InternalError(err)
+	}
+
+	// Get the user stories
+	q = stories.Where("user_id=?", user.Id).Limit(50).Order("created_at desc")
+	userStories, err := stories.FindAll(q)
 	if err != nil {
 		return router.InternalError(err)
 	}
@@ -31,6 +39,7 @@ func HandleShow(context router.Context) error {
 	view := view.New(context)
 	view.AddKey("user", user)
 	view.AddKey("comments", userComments)
+	view.AddKey("stories", userStories)
 	view.AddKey("meta_title", user.Name)
 	view.AddKey("meta_desc", user.Name)
 	return view.Render()
