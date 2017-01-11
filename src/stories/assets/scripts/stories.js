2f0865ddcad61fc2a7a6b7dce95a3177aec94d36
Kenny Grant <kennygrant@gmail.com>
2017-01-11 23:14:29 +0000

    Adjusted create page to fetch metadata from github

--- src/stories/assets/scripts/stories.js
+++ src/stories/assets/scripts/stories.js
@@ -11,16 +11,60 @@ DOM.Ready(function() {
 function SetSubmitStoryName() {
 
   DOM.On('.active_url_field', 'change', function(e) {
-    var field = DOM.First('.active_name_field')
-    if (field.value == "") {
-      field.value = urlToSentenceCase(this.value);
+    var name = DOM.First('.name_field')
+    var summary = DOM.First('.summary_field')
+    var original_name = name.value
+    var original_url = this.value
+    
+    // First locally fill in the name field 
+    if (original_name== "") {
+      
+      // For github urls, try to fetch some more info 
+      if (original_url.startsWith('https://github.com/')) {
+      
+        var url = original_url.replace('https://github.com/','https://api.github.com/repos/')
+         DOM.Get(url,function(request){
+           var data = JSON.parse(request.response);
+          
+           // if we got a reponse, try using it. 
+           name.value =  data.name + " - " + data.description
+           summary.value = data.description + " by " + data.owner.login
+           
+           // later use 
+           // created_at -> original_published_at
+           // updated_at -> original_updated_at
+           // data.owner.name -> original_author
+           
+         },function(){
+           console.log("failed to fetch github data")
+         });
+      
+        return false;
+      } 
+      
+      // We could also attempt to fetch the html page, and grab metadata from it 
+      // author, pubdate, metadesc etc
+      // would this be better done in a background way after story submission?
+      
+      
+      // Else just use name from local url if we can
+      name.value = urlToSentenceCase(original_url);
+    
+  
     }
+  
+    
+    
   });
 
 }
 
 // Change a URL to a sentence for SetSubmitStoryName
 function urlToSentenceCase(url) {
+  if (url === undefined) {
+    return ""
+  }
+  
   var parts, name
   url = url.replace(/\/$/, ""); // remove trailing /
   parts = url.split("/"); // now split on /
