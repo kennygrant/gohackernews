4a85b1c5ca73c62fa71444ef53e9f6924b03a699
Kenny Grant <kennygrant@gmail.com>
2017-04-13 00:41:54 +0100

    Added mutex to stats package

--- src/lib/stats/stats.go
+++ src/lib/stats/stats.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -18,6 +19,7 @@ var PurgeInterval = time.Minute * 5
 // obviously an in-memory store is not suitable for very large sites
 // but for smaller sites with a few hundred concurrent users it's fine
 var identifiers = make(map[string]time.Time)
+var mu sync.RWMutex
 
 // RegisterHit registers a hit and ups user count if required
 func RegisterHit(r *http.Request) {
@@ -47,20 +49,26 @@ func RegisterHit(r *http.Request) {
 	id := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
 
 	// Insert the entry with current time
+	mu.Lock()
 	identifiers[id] = time.Now()
-
+	mu.Unlock()
 }
 
 // HandleUserCount serves a get request at /stats/users/count
 func HandleUserCount(w http.ResponseWriter, r *http.Request) error {
+
 	// Render json of our count for the javascript to display
+	mu.RLock()
 	json := fmt.Sprintf("{\"users\":%d}", len(identifiers))
+	mu.RUnlock()
 	_, err := w.Write([]byte(json))
 	return err
 }
 
 // UserCount returns a count of users in the last 5 minutes
 func UserCount() int {
+	mu.RLock()
+	defer mu.RUnlock()
 	return len(identifiers)
 }
 
@@ -72,12 +80,14 @@ func init() {
 // purgeUsers clears the users list of users who last acted PurgeInterval ago
 func purgeUsers() {
 
+	mu.Lock()
 	for k, v := range identifiers {
 		purgeTime := time.Now().Add(-PurgeInterval)
 		if v.Before(purgeTime) {
 			delete(identifiers, k)
 		}
 	}
+	mu.Unlock()
 
 	time.AfterFunc(time.Second*60, purgeUsers)
 
