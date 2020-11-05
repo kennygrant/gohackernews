886791fd129d21d2d9a9be3e9708ef4e450ee58f
Kenny Grant <kennygrant@gmail.com>
2020-11-05 11:19:03 +0000

    Adjust variable names for clarity

--- src/app/assets/scripts/adom.js
+++ src/app/assets/scripts/adom.js
@@ -3,7 +3,7 @@
 var DOM = (function() {
     return {
 
-        // Apply a function on document ready
+        // Apply a function f on document ready
         Ready: function(f) {
             if (document.readyState != 'loading') {
                 f();
@@ -12,49 +12,49 @@ var DOM = (function() {
             }
         },
 
-        // Attach event handlers to all matches for a selector 
-        On: function(s, b, f) {
-            DOM.Each(s, function(el, i) {
-                el.addEventListener(b, f);
-            });
-        },
-        
-        // Return true if any elements match selector
-        Exists: function(s) {
-            return (document.querySelector(s) !== null);
+        // Return true if any elements match selector sel
+        Exists: function(sel) {
+            return (document.querySelector(sel) !== null);
         },
 
         // Return a NodeList of elements matching selector
-        All: function(s) {
-            return document.querySelectorAll(s);
+        All: function(sel) {
+            return document.querySelectorAll(sel);
         },
 
         // Return the first in the NodeList of elements matching selector - may return nil
-        First: function(s) {
-            return DOM.All(s)[0];
+        First: function(sel) {
+            return DOM.All(sel)[0];
         },
 
         // Apply a function to elements of an array
-        ForEach: function(a, f) {
-            Array.prototype.forEach.call(a, f);
+        ForEach: function(array, f) {
+            Array.prototype.forEach.call(array, f);
         },
 
         // Apply a function to elements matching selector, return true to break
-        Each: function(s, f) {
-            var a = DOM.All(s);
-            for (i = 0; i < a.length; ++i) {
-              f(a[i], i);
+        Each: function(sel, f) {
+            var array = DOM.All(sel);
+            for (i = 0; i < array.length; ++i) {
+              f(array[i], i);
             }
         },
 
+        // Attach event handlers to all matches for a selector 
+        On: function(sel, name, f) {
+            DOM.Each(sel, function(el, i) {
+                el.addEventListener(name, f);
+            });
+        },
+        
         // Return a NodeList of nearest elements matching selector, 
         // checking children, siblings or parents of el
-        Nearest: function(el, s) {
+        Nearest: function(el, sel) {
 
             // Start with this element, then walk up the tree till 
             // we find a child which matches selector or we run out of elements
             while (el !== undefined && el !== null) {
-                var nearest = el.querySelectorAll(s);
+                var nearest = el.querySelectorAll(sel);
                 if (nearest.length > 0) {
                     return nearest;
                 }
@@ -65,39 +65,39 @@ var DOM = (function() {
         },
 
         // Attribute returns either an attribute value or an empty string (if null)
-        Attribute: function(el, a) {
-            if (el.getAttribute(a) === null) {
+        Attribute: function(el, name) {
+            if (el.getAttribute(name) === null) {
                 return ''
             }
-            return el.getAttribute(a)
+            return el.getAttribute(name)
         },
 
         // HasClass returns true if this element has this className
-        HasClass: function(el, c) {
-            var regexp = new RegExp("\\b" + c + "\\b", 'gi');
+        HasClass: function(el, name) {
+            var regexp = new RegExp("\\b" + name + "\\b", 'gi');
             return regexp.test(el.className);
         },
 
         // AddClass Adds the given className from el.className
         // s may be a string selector or an element
-        AddClass: function(s, c) {
+        AddClass: function(s, name) {
             if (typeof s === "string") {
                 DOM.Each(s, function(el, i) {
-                    if (!DOM.HasClass(el, c)) {
-                        el.className = el.className + ' ' + c;
+                    if (!DOM.HasClass(el, name)) {
+                        el.className = el.className + ' ' + name;
                     }
                 });
             } else {
-                if (!DOM.HasClass(s, c)) {
-                    s.className = s.className + ' ' + c;
+                if (!DOM.HasClass(s, name)) {
+                    s.className = s.className + ' ' + name;
                 }
             }
         },
 
         // RemoveClass removes the given className from el.className
         // s may be a string selector or an element
-        RemoveClass: function(s, c) {
-            var regexp = new RegExp("\\b" + c + "\\b", 'gi');
+        RemoveClass: function(s, name) {
+            var regexp = new RegExp("\\b" + name + "\\b", 'gi');
             if (typeof s === "string") {
                 DOM.Each(s, function(el, i) {
                     el.className = el.className.replace(regexp, '')
@@ -109,54 +109,54 @@ var DOM = (function() {
 
         // Format returns the format string with the indexed arguments substituted
         // Formats are of the form - "{0} {1}" which uses variables 0 and 1 respectively
-        Format: function(f) {
+        Format: function(format) {
             for (var i = 1; i < arguments.length; i++) {
                 var regexp = new RegExp('\\{' + (i - 1) + '\\}', 'gi');
-                f = f.replace(regexp, arguments[i]);
+                format = format.replace(regexp, arguments[i]);
             }
-            return f;
+            return format;
         },
 
         // Hide elements matching selector 
         // s may be a string selector or an element
-        Hide: function(s) {
-            if (typeof s === "string") {
-                DOM.Each(s, function(el, i) {
+        Hide: function(sel) {
+            if (typeof sel === "string") {
+                DOM.Each(sel, function(el, i) {
                     el.style.display = 'none';
                 });
             } else {
-                s.style.display = 'none';
+                sel.style.display = 'none';
             }
         },
 
         // Show elements matching selector
         // s may be a string selector or an element
-        Show: function(s) {
-            if (typeof s === "string") {
-                DOM.Each(s, function(el, i) {
+        Show: function(sel) {
+            if (typeof sel === "string") {
+                DOM.Each(sel, function(el, i) {
                     el.style.display = '';
                 });
             } else {
-                s.style.display = '';
+                sel.style.display = '';
             }
         },
 
         // Hidden returns true if this element is hidden
         // s may be a string selector or an element
-        Hidden: function(s) {
-            if (typeof s === "string") {
-                return (DOM.First(s).style.display == 'none');
+        Hidden: function(sel) {
+            if (typeof sel === "string") {
+                return (DOM.First(sel).style.display == 'none');
             } else {
-                return s.style.display == 'none';
+                return sel.style.display == 'none';
             }
 
         },
 
         // Toggle the Shown or Hidden value of elements matching selector
         // s may be a string selector or an element
-        ShowHide: function(s) {
-            if (typeof s === "string") {
-                DOM.Each(s, function(el, i) {
+        ShowHide: function(sel) {
+            if (typeof sel === "string") {
+                DOM.Each(sel, function(el, i) {
                     if (el.style.display != 'none') {
                         el.style.display = 'none';
                     } else {
@@ -164,34 +164,18 @@ var DOM = (function() {
                     }
                 });
             } else {
-                if (s.style.display != 'none') {
-                    s.style.display = 'none';
+                if (sel.style.display != 'none') {
+                    sel.style.display = 'none';
                 } else {
-                    s.style.display = '';
+                    sel.style.display = '';
                 }
             }
         },
 
-        // Ajax - Send to url u the data d, call fs for success, fe for failures
-        Post: function(u, d, fs, fe) {
+        // Ajax - Get the data from url, call fs for success, fe for failures
+        Get: function(url, fs, fe) {
             var request = new XMLHttpRequest();
-            request.open('POST', u, true);
-            request.onerror = fe;
-            request.onload = function() {
-                if (request.status >= 200 && request.status < 400) {
-                    fs(request);
-                } else {
-                    fe(request);
-                }
-            };
-            request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-            request.send(d);
-        },
-
-        // Ajax - Get the data from url u, call fs for success, fe for failures
-        Get: function(u, fs, fe) {
-            var request = new XMLHttpRequest();
-            request.open('GET', u, true);
+            request.open('GET', url, true);
             request.onload = function() {
                 if (request.status >= 200 && request.status < 400) {
                     fs(request);
@@ -201,7 +185,25 @@ var DOM = (function() {
             };
             request.onerror = fe;
             request.send();
+        },
+
+        // Ajax - post the data to url, call fs for success, fe for failures
+        Post: function(url, data, fs, fe) {
+            var request = new XMLHttpRequest();
+            request.open('POST', url, true);
+            request.onerror = fe;
+            request.onload = function() {
+                if (request.status >= 200 && request.status < 400) {
+                    fs(request);
+                } else {
+                    fe(request);
+                }
+            };
+            request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+            request.send(data);
         }
+
+        
 
     };
 
